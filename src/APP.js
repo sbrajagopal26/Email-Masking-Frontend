8b76d05ef61ecad77f41d9340a281c586301c1bd
sbrajagopal26 <sbrajagopal26@users.noreply.github.com>
2025-08-05 23:08:56 +0200

    Create APP.js

---
+++ src/APP.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmailMasker from './components/EmailMasker';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Email Privacy Masker</h1>
+      <EmailMasker />
+    </div>
+  );
+}
+
+export default App;
