f29bcd7b9f3540fd28f82487f2e31166a8ffc944
sbrajagopal26 <sbrajagopal26@users.noreply.github.com>
2025-08-06 13:23:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
+// App.js
 import React from 'react';
-import EmailMasker from './components/EmailMasker';
 import LandingPage from "./LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Email Privacy Masker</h1>
-      <EmailMasker />
       <LandingPage />
     </div>
   );
